b188b96d84eb2d4304509706fcdf8c4feb92443e
haseeb zafar <Haseebzafar107@gmail.com>
2021-06-22 23:00:37 +0500

    test

--- service/database.js
+++ service/database.js
@@ -20,7 +20,7 @@ const mysql = require('mysql2');
 // OUR DATABASE
 
 const pool = mysql.createPool({
-    host: '170.249.211.50',
+    host: 'https://www.meemuskitchenpk.com/',
     user: 'codemine_pos',
     database: 'codemine_pos',
     password: 'pos123456'
