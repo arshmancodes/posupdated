a95aa1a6a9b77e6dba278b28a2f862a69850da21
haseeb zafar <Haseebzafar107@gmail.com>
2021-06-23 14:36:21 +0500

    test dvb

--- service/database.js
+++ service/database.js
@@ -22,6 +22,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '170.249.211.50',
     user: 'codemine_pos',
+    port: 3306,
     database: 'codemine_pos',
     password: 'pos123456',
     insecureAuth: true
